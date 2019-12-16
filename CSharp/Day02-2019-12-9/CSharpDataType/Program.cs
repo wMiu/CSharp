875e95ebba2ed5ce0a2373983c506abc5a9e55c9
wMiu <miu0619@163.com>
2019-12-16 12:00:00 +0800

    更改Start.md连接描述文字；补充Day02 - Object Type

--- CSharp/Day02-2019-12-9/CSharpDataType/Program.cs
+++ CSharp/Day02-2019-12-9/CSharpDataType/Program.cs
@@ -6,6 +6,19 @@ namespace CSharpDataType
     {
         static void Main(string[] args)
         {
+
+            /*
+            * bool 布尔值
+            * byte 0 ~ 255 整数 | sbyte -128 ~ 127
+            * char Unicode 字符
+            * float 单精度浮点数
+            * double 双精度浮点数
+            * decimal 高精度浮点数
+            * int 32位整数 | uint 32位正整数
+            * long 大整数64位 | ulong 64位正整数
+            * short 16位整数 | ushort 0 ~ 65535 整数
+            */
+
             // 类型转换 与 C# 的数据类型
             double a = 1.111;
             int b;
@@ -22,18 +35,20 @@ namespace CSharpDataType
             Console.WriteLine("Bool 转换为 Int， Ture = {0}; Bool 转换为 Double， Fales = {1}", e, f);
             // -> 0, 1 | 将Bool类型转换为Double还是 0 / 1
 
-            /*
-            * bool 布尔值
-            * byte 0 ~ 255 整数 | sbyte -128 ~ 127
-            * char Unicode 字符
-            * float 单精度浮点数
-            * double 双精度浮点数
-            * decimal 高精度浮点数
-            * int 32位整数 | uint 32位正整数
-            * long 大整数64位 | ulong 64位正整数
-            * short 16位整数 | ushort 0 ~ 65535 整数
-            */
+            // sizeof(type) : 返回给定类型的变量所占用的字节数
+            // 官方文档 : https://docs.microsoft.com/zh-cn/dotnet/csharp/language-reference/operators/sizeof
+            Console.WriteLine("int sizeof : {0}", sizeof(int));  // -> 4
+            Console.WriteLine("double sizeof : {0}", sizeof(double));  // -> 8
+            Console.WriteLine("bool sizeof : {0}", sizeof(bool));  // -> 1
             
+            //Object Type - 对象类型
+            object obj;
+            // 装箱 - 将一个值类型隐式转换为对象类型称之为装箱。
+            int i = 1;
+            obj = i;
+            obj = (object)i;
+            // 拆箱 - 将一个对象类型转换为值类型称之为拆箱。
+            int j = (int)obj;
         }
     }
 }
--- CSharp/Start.md
+++ CSharp/Start.md
@@ -3,6 +3,6 @@
 #### 网站
 
 [菜鸟教程-C#](https://www.runoob.com/csharp/csharp-tutorial.html)  
-[C#变成指南-官方文档](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/index)  
+[C#指南-官方文档](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/index)  
 [编译器-VS/VSCode](https://visualstudio.microsoft.com/zh-hans/downloads/)  
 [跨平台-mono](https://www.mono-project.com/)  

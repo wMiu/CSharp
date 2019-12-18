c56106a9aff1ce8baed36c73ce0ce72e16d9df63
wMiu <miu0619@163.com>
2019-12-18 16:27:07 +0800

    增加 If , if else , if...else if...else

--- CSharp/Day05-2019-12-18/judge/Program.cs
+++ CSharp/Day05-2019-12-18/judge/Program.cs
@@ -13,14 +13,45 @@ namespace judge
                 }
             */
             int a = 1;  // 局部变量 - 只有在此作用域下才能使用
+            Console.WriteLine("a = {0}", a);
             if (a == 1) {
-                Console.WriteLine("a = {0}", a);
+                Console.WriteLine("a 等于 1");
             }  
 
             // if else 语句
             /*
-
+                if (boolean_expression){
+                    如果 布尔表达式为为真则执行此处代码块
+                } else {
+                    如果为假则执行此处代码块
+                }
             */       
+            if (a == 2) {
+                Console.WriteLine("a 等于 2");
+            } else {
+                Console.WriteLine("a 不等于 2");
+            }
+            // if ... else if ... else 语句
+            /*
+                if (boolean_expression1){
+                    如果布尔表达式1 为真则执行此处代码块
+                } else if (boolean_expression2){
+                    如果布尔表达式2 为真则执行此处代码块
+                } ... {
+                    ...
+                } else {
+                    以上表达式全为假则执行此处代码块
+                }
+            */
+            if (a == 2){
+                Console.WriteLine("a 等于 2");
+            } else if (a == 3){
+                Console.WriteLine("a 等于 3");
+            } else {
+                Console.WriteLine("a 不等于 2 ，也不等于 3");
+            }
+            // 注意 - 一个if...else if...else 语句最多且只有一个else,并且出现在最后
+            //      - 在上面的条件成立后将不会执行下面的其他条件了。
         }
     }
 }

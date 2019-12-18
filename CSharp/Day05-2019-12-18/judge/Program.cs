b39be084787fdd4432819b61215b7dd11def4a0d
wMiu <miu0619@163.com>
2019-12-18 16:16:25 +0800

    初始化Day05 判断

---
+++ CSharp/Day05-2019-12-18/judge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace judge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // if 语句
+            /*
+                if (boolean_expression){
+                    如果boolean_expression为真则执行语句块
+                }
+            */
+            int a = 1;  // 局部变量 - 只有在此作用域下才能使用
+            if (a == 1) {
+                Console.WriteLine("a = {0}", a);
+            }  
+
+            // if else 语句
+            /*
+
+            */       
+        }
+    }
+}

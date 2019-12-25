086145e9d7f964a65552a2fa4a97cc220f82c540
wMiu <miu0619@163.com>
2019-12-25 17:32:33 +0800

    初始化 - SelectSort 选择排序

---
+++ MiniMoudle/Sort/SelectSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int[] arr = new int[]{2, 11, 9, 1, 89, 7, 6, 2};
+            int[] sort_arr = p.SelectSort(arr);
+            foreach (int i in sort_arr){
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+        }
+        public int[] SelectSort(int[] arr){
+            int len = arr.Length;
+            int i, j, med, min;
+            for (i = 0; i < len; i ++){
+                for (j = i + 1; j < len; j ++){
+                    min = i;
+                    if (arr[min] > arr[j]){
+                        min = j;
+                    }
+                }
+                med = arr[j];
+                arr[j] = arr[i];
+                arr[i] = med;
+            }
+            return arr;
+        }
+    }
+}

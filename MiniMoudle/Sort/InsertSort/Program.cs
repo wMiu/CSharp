a9a334fe67cc8d2010e96e897be78e82d8124094
wMiu <miu0619@163.com>
2020-01-02 09:50:00 +0800

    增加InsertSort 与调试代码。

---
+++ MiniMoudle/Sort/InsertSort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace InsertSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int[] arr = new int[]{2, 5, 1, 7, 9, 3};
+            int[] sort_arr = p.insertSort(arr);
+            foreach (int i in sort_arr){
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+
+        }
+        public int[] insertSort(int[] arr){
+            int len = arr.Length;
+            int i, j, now;
+            for (i = 1; i < len; i++){
+                j = i - 1;
+                now = arr[i];
+                while (j >= 0){
+                    if (now < arr[j]){
+                        arr[j + 1] = arr[j];
+                        j--;
+                    } else {
+                        break;
+                    }
+                }
+                arr[j + 1] = now;
+            }
+            return arr;
+        }
+    }
+}
